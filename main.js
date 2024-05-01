b453b48dd26ce5223935f00acbf5c8e13f839ca7
Gabriel Lira <gabriel.tertulino@proton.me>
2024-05-01 10:23:45 -0300

    commit 4.0

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const form = document.querySelector('.container__form');
-const emailAddress = document.querySelector('form input[type="email"]');
-const error = document.
+const emailInput = document.querySelector('input[type="email"]');
+const errorText = document.querySelector('.form_error-text');
+const errorIcon = document.querySelector('.form__error-icon');
+
+function checkEmail (event) {
+  if(!emailInput.validity.valid) {
+    event.preventDefault();
+    errorText.innerText = "Please provide a valid email"
+    errorIcon.style.display = "block";
+    emailInput.style.borderColor = "#F96464";
+    emailInput.focus();
+    console.log('Function Working');
+  }
+
+}
+
+
+form.addEventListener('submit', checkEmail);
